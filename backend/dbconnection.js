5b6848a09eae3ba5a585b5e8ea47211a3fd0adff
taskeenkhan-bscs <taskoowzr@example.com>
2026-07-07 15:21:52 +0500

    Fix MongoDB connection

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
-import dns from "dns" 
+import dns from "dns";
 
-const dbconnection = async () => { 
-  dns.setServers(["1.1.1.1", "8.8.8.8"])
+const dbconnection = async () => {
+  dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
   try {
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("✅ MongoDB Atlas Connected");
+    console.log("Connection State:", mongoose.connection.readyState);
   } catch (error) {
-    console.log("DB Error:", error.message);
+    console.error("❌ MongoDB Connection Error:");
+    console.error(error);
   }
 };
 
